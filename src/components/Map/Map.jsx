ebba394b724cf9aaed51689a8f7e1b19eb6d7b69
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-03-09 23:52:44 +0100

    map section

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import googleMapReact from "google-map-react";
+import {paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import { Rating } from "@material-ui/lab";
+
+
+import useStyles from './styles'
 
 const Map = () => {
+  const classes = useStyles();
+  const isMoblie = useMediaQuery('(min-width:600px)');
+
+  const coordinates = { lat: 0,}
   return (
-    <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+            bootstrapURLKeys={{ key: ''}}
+            defaultCenter={}
+            >
+        
+
+      </GoogleMapReact>
+    </div>
   );
 }
 
